753b227c7f23eb17cae64ff5bafa7b85811eafd0
Eugene Ong <eugeneong2013@gmail.com>
2017-04-10 02:55:54 +0800

    Clean Home Page

--- CarInsuranceApplication/App_Start/RouteConfig.cs
+++ CarInsuranceApplication/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace CarInsuranceApplication
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Policies", action = "Step1", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
